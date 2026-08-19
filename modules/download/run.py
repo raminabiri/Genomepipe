03a534d3e88bf887ce3e90ce4b80c7054b3faf10
raminabiri <ramin.abiri@gmail.com>
2026-08-19 16:25:36 +0330

    Route download runner through multi-database dispatcher

--- modules/download/run.py
+++ modules/download/run.py
@@ -1,28 +1,5 @@
-from .helper import read_config
-from .helper import make_dir
-from .ncbi import NCBI
+"""Compatibility entry point for the multi-database download layer."""
 
+from .download import run
 
-def run(project_path):
-
-    config = read_config(project_path / "config" / "project.yaml")
-
-    genome_dir = config["input"]["genomes"]
-
-    make_dir(genome_dir)
-
-    organism = config["project"]["organism"]
-
-    use_existing = input("\nUse existing genomes? (y/n): ").lower()
-
-    if use_existing == "y":
-        return
-
-    if input("Download from NCBI? (y/n): ").lower() == "y":
-
-        ncbi = NCBI(
-            organism,
-            genome_dir,
-        )
-
-        ncbi.run()
+__all__ = ["run"]
